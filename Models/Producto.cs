b2e1a627661a8955093b7c4c99da1b37cca4ea4c
Luis Torres <ltorcerps4@gmail.com>
2020-11-10 15:38:11 -0500

    tarea

---
+++ Models/Producto.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Tareaprogramacion.Models
+{
+    public class Producto
+    {
+        public string nombre {get; set;}
+
+        public double precio {get; set;}
+
+        public string descripcion {get; set;}
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# actividadenclase
-actividad
+# Tareaprogramacion
